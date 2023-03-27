c222645d8e7bc1113a074511d171205c62053062
Ogada Moses <ogadamoses59@gmail.com>
2023-03-27 18:12:30 +0300

    start add vehicle functionality

---
+++ src/components/AddVehicle.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import Dialog  from '@mui/material/Dialog';
+import DialogActions  from '@mui/material/DialogActions';
+import DialogContent  from '@mui/material/DialogContent';
+import DialogTitle  from '@mui/material/DialogTitle';
+
+function AddVehicle (props) {
+    const [open, setOpen] = useState(false);
+    const [vehicle, setVehicle] = useState ( {
+        brand: '',
+        model: '',
+        color: '',
+        vyear:'',
+        price: '',
+    });
+    const handleClickOpen = () => {
+        setOpen (true);
+    };
+
+    const handleClose = () => {
+        setOpen (false);
+    };
+
+    
+    return (
+        <div>
+
+        </div>
+
+    );
+}
+
+export default AddVehicle;
