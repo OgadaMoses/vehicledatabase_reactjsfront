0306ca839083c5e98479e5f318015dbda7a89b3c
Ogada Moses <ogadamoses59@gmail.com>
2023-04-05 09:10:45 +0300

    add save and cancel buttons

--- src/components/EditVehicle.js
+++ src/components/EditVehicle.js
@@ -18,4 +18,49 @@ function EditVehicle (props) {
     const handleClose = () => {
         setOpen(false);
     };
+
+    const handleChange = (event) => {
+        setVehicle({...vehicle,
+        [event.target.name]: event.target.value});
+    }
+
+    const handleSave = () => {
+
+    }
+
+    return (
+        <div>
+            <button onClick={handleClickOpen}>Edit</button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Edit Vehicle</DialogTitle>
+                <DialogContent>
+                    <input placeholder="Brand" name="brand"
+                    value={vehicle.brand} onChange={handleChange}
+                    /><br/>
+
+                  <input placeholder="Model" name="model"
+                    value={vehicle.brand} onChange={handleChange}
+                    /><br/>
+
+                  <input placeholder="Color" name="color"
+                    value={vehicle.brand} onChange={handleChange}
+                    /><br/>
+
+                  <input placeholder="Year" name="vyear"
+                    value={vehicle.brand} onChange={handleChange}
+                    /><br/>
+
+                  <input placeholder="Price" name="price"
+                    value={vehicle.brand} onChange={handleChange}
+                    /><br/>
+                </DialogContent>
+                <DialogActions>
+                    <button onClick={handleClose}>Cancel</button>
+                    <button  onClick={handleSave}>Save</button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
 }
+
+export default EditVehicle;
