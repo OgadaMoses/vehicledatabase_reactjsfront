04feef2dd6f86f5bde49e16dfdf7c4fccd7b5209
Ogada Moses <ogadamoses59@gmail.com>
2023-04-25 09:10:21 +0300

    Add snapshot testing to monitor UI changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,13 @@
 import {render, screen, fireEvent} from '@testing-library/react';
 import App from './App';
+import TestRenderer from 'react-test-renderer';
+import AddVehicle from './components/AddVehicle';
+
+test('renders a snapshot', () => {
+  const tree = TestRenderer.create
+        (<AddVehicle/>).toJSON();
+    expect(tree).toMatchSnapshot();    
+});
 
 test('open add vehicle modal form',async () =>{
      render (<App />);
