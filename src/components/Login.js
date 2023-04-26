b8bf3a995930b39e56d95717e5934c4946b2d47a
Ogada Moses <ogadamoses59@gmail.com>
2023-04-26 08:19:37 +0300

    add MU to login UI

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,42 @@
 import React, {useState} from 'react';
 import {SERVER_URL} from '../constants.js';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
 
-function LOgin
+function Login (){
+    const [user, setUser] = useState({
+        username: '',
+        password: ''
+    });
+    const [isAuthenticated, setAuth] = useState(false);
+
+    const handleChange = (event) => {
+        setUser({...user,
+        [event.target.name]: event.target.value});
+    }
+
+    return (
+        <div> 
+            <Stack spacing={2} alignItems='center' mt={2}>
+                <TextField
+                name="username"
+                label="Username"
+                onChange={handleChange} />
+                <TextField
+                 type="password"
+                 name="password"
+                 label="Password"
+                 onChange={handleChange}/>
+                <Button 
+                 variant="outlined"
+                 color="primary"
+                 onClick={login}>
+                    Login
+                 </Button>
+            </Stack>
+        </div>
+    );
+} 
+
+export default Login;
